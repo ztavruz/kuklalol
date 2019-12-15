71062f4eeadd2fdb208db4a049eaf159ab4cde46
ztavruz <mr.ztavruz1987@mail.ru>
2019-12-15 21:35:02 +0300

    header ready at 45%

--- view/main/index.php
+++ view/main/index.php
@@ -1,32 +1,31 @@
-<div class="header br_000">
-  <div class="wrapper br_000">
-    <div class="row_top br_000">
-      <div class="logo">
-        <h1>KUKLALOL</h1>
+<div class="header  ">
+
+  <div class="wrapper columns ">
+
+    <div class="row_top rows">
+      <div class="wrapper_logo rows">
+        <img class="logo " src="/content/img/logo.png" alt="">
       </div>
-      <div class="block_contacts  br_000">
-        <div class="phone">
-          <a href="tel:+380985554433">+7(999) 969-50-31</a>
-          <div class="pnote_signature">10:00 до 20:00 по МСК</div>
+      <div class="block_contacts rows  ">
+        <div class="block_phone columns ">
+          <a href="tel:+380985554433" class="phone ">+7(999) 999-99-99</a>
+          <p class="pnote_signature ">10:00 до 20:00 по МСК</p>
         </div>
-        <div class="order_call">
-          <div class="button">Заказать звонок</div>
-        </div>
-        <div class="whatsapp_btn">
-          <div class="button">вацап</div>
-        </div>
+        <button class="btn btn-primary order_call ">Заказать звонок</button>
+        <button class="watsap ">Вац</button>
       </div>
     </div>
-    <div class="row_bottom br_000">
-      <div class="block_links">
-        <a href="#">Главная</a>
-        <a href="#">Все бренды</a>
-        <a href="#">Доставка</a>
-        <a href="#">О магазине</a>
-        <a href="#">Блог</a>
-      </div>
+
+    <div class="row_bottom rows ">
+      <a href="#">Главная</a>
+      <a href="#">Все бренды</a>
+      <a href="#">Доставка</a>
+      <a href="#">О магазине</a>
+      <a href="#">Блог</a>
     </div>
+
   </div>
+
 </div>
 <div class="content">
 
